753acc0cd6109a1dd4b5340e513e51d181ffcd2d
Pietro Ditta <pietro.ditta@jakala.com>
2025-03-22 12:35:08 +0100

    added scripts and refactored

---
+++ scripts/delete_release.py
@@ -0,0 +1,55 @@
+import os
+import requests
+import sys
+
+def get_release_id(tag_to_delete, github_repository, github_token):
+    url = f"https://api.github.com/repos/{github_repository }/releases/tags/{tag_to_delete}"
+    headers = {
+        "Authorization": f"Bearer {github_token}"
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    releases = response.json()
+
+    return releases['id']
+
+
+def delete_release(github_repository, release_id, github_token):
+    url = f"https://api.github.com/repos/{github_repository}/releases/{release_id}"
+    headers = {
+        "Authorization": f"Bearer {github_token}"
+    }
+    response = requests.delete(url, headers=headers)
+    return response.status_code
+
+
+
+def main():
+    git_hub_repository = os.getenv('GITHUB_REPOSITORY')
+    tag_to_delete = os.getenv('TAG_TO_DELETE')
+    github_token = os.getenv('GITHUB_TOKEN')
+
+    if not git_hub_repository or not tag_to_delete or not github_token:
+        print("Errore: Assicurati che le variabili d'ambiente GITHUB_REPOSITORY, TAG_TO_DELETE e GITHUB_TOKEN siano impostate.")
+        sys.exit(1)
+
+    release_id = get_release_id(tag_to_delete, git_hub_repository, github_token)
+    
+    if release_id:
+        print(f"La release associata al tag {tag_to_delete} esiste, procedo con la cancellazione.")
+        status_code = delete_release(github_token, release_id, github_token)
+        
+        if status_code == 204:
+            print(f"La release associata al tag {tag_to_delete} è stata cancellata con successo.")
+        elif status_code == 404:
+            print(f"Errore: La release associata al tag {tag_to_delete} non è stata trovata per la cancellazione.")
+            sys.exit(1)
+        else:
+            print(f"Errore nella cancellazione. Codice di stato: {status_code}")
+            sys.exit(1)
+    else:
+        print(f"La release associata al tag {tag_to_delete} non esiste, niente da cancellare.")
+
+
+if __name__ == "__main__":
+    main()
