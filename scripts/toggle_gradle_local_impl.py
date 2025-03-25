711a5cc40a9fc9a30614dbe7508a068910138ca8
Pietro Ditta <pietro.ditta@jakala.com>
2025-03-25 10:30:45 +0100

    fixed

--- scripts/toggle_gradle_local_impl.py
+++ scripts/toggle_gradle_local_impl.py
@@ -5,15 +5,13 @@ from gradle_utils import update_gradle_properties
 def main():
     parser = argparse.ArgumentParser(description='Print Gradle Properties')
     parser.add_argument('--gradleFileName', type=str, help='The gradle file name')
-    parser.add_argument('--enable', type=bool, default='false', help='The debug option is enabled')
+    parser.add_argument('--enable', type=str, choices=['true', 'false'], help='Enable local implementation')
 
     args = parser.parse_args()
 
     gradle_file_name = args.gradleFileName
     properties = read_gradle_properties(gradle_file_name)
-    enabled = args.enable == 'true'
-
-    print(enabled)
+    enabled = args.enable.lower() == 'true'
 
     if(enabled):
         properties['ENABLE_LOCAL_IMPLEMENTATION'] = 'true'
