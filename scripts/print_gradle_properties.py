2c54f2f117424b09c09704ada5993dcab5c35631
Pietro Ditta <pietro.ditta@jakala.com>
2025-03-25 12:10:37 +0100

    refactored

--- scripts/print_gradle_properties.py
+++ scripts/print_gradle_properties.py
@@ -12,6 +12,8 @@ def main():
    
     properties = read_gradle_properties(gradle_file_name)
     
+    is_first_release = properties.get('IS_FIRST_RELEASE', 'N/A')
+    is_first_maven_release = properties.get('IS_FIRST_MAVEN_RELEASE', 'N/A')
     app_demo_version_code = properties.get('APP_DEMO_VERSION_CODE', 'N/A')
     release_version_name = properties.get('RELEASE_VERSION_NAME', 'N/A')
     build_maven_sdk_version = properties.get('BUILD_MAVEN_SDK_VERSION', 'N/A')
@@ -20,7 +22,8 @@ def main():
     print(f"APP_DEMO_VERSION_CODE={app_demo_version_code}")
     print(f"RELEASE_VERSION_NAME={release_version_name}")
     print(f"BUILD_MAVEN_SDK_VERSION={build_maven_sdk_version}")
-    print(f"ENABLE_LOCAL_IMPLEMENTATION={enable_local_impl}")
+    print(f"IS_FIRST_RELEASE={is_first_release}")
+    print(f"IS_FIRST_MAVEN_RELEASE={is_first_maven_release}")
 
 if __name__ == "__main__":
     main()
