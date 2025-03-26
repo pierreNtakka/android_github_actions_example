ce304c750efe4fff6124433d12d607c53de85bf7
Pietro Ditta <pietro.ditta@jakala.com>
2025-03-26 13:15:17 +0100

    fix scripts error

--- scripts/inputs_verification.py
+++ scripts/inputs_verification.py
@@ -11,7 +11,7 @@ def main():
     args = parser.parse_args()
     uploadAppDemo = args.uploadAppDemo.lower() == 'true'
     uploadSdk = args.uploadSdk.lower() == 'true'
-    incrementVersion = args.enable.lower() == 'true'
+    incrementVersion = args.incrementVersion.lower() == 'true'
     
     if not incrementVersion and not uploadAppDemo and not uploadSdk:
         print("Errore: Non è possibile NON incrementare la versione dell'app e non effettuare nemmeno un upload")
